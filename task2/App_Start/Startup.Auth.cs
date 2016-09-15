fd8018c5519dc04cc0383a80f9ab44513fd62222
Denis <starwars92@yandex.ru>
2016-09-15 09:06:38 +0300

    first commit

---
+++ task2/App_Start/Startup.Auth.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using task2.Models;
+
+namespace task2
+{
+    public partial class Startup
+    {
+        // Дополнительные сведения о настройке проверки подлинности см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Настройка контекста базы данных, диспетчера пользователей и диспетчера входа для использования одного экземпляра на запрос
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Включение использования файла cookie, в котором приложение может хранить информацию для пользователя, выполнившего вход,
+            // и использование файла cookie для временного хранения информации о входах пользователя с помощью стороннего поставщика входа
+            // Настройка файла cookie для входа
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    // Позволяет приложению проверять метку безопасности при входе пользователя.
+                    // Эта функция безопасности используется, когда вы меняете пароль или добавляете внешнее имя входа в свою учетную запись.  
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });            
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Позволяет приложению временно хранить информацию о пользователе, пока проверяется второй фактор двухфакторной проверки подлинности.
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+            // Позволяет приложению запомнить второй фактор проверки имени входа. Например, это может быть телефон или почта.
+            // Если выбрать этот параметр, то на устройстве, с помощью которого вы входите, будет сохранен второй шаг проверки при входе.
+            // Точно так же действует параметр RememberMe при входе.
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+            // Раскомментируйте приведенные далее строки, чтобы включить вход с помощью сторонних поставщиков входа
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            app.UseTwitterAuthentication(
+               consumerKey: "PFufESDiX2dBnH3fM32an6WWC",
+               consumerSecret: "NWH8Qm74ZAwIN4XiL5lgTuTyuhbIVLCRJxn5WENniFahBpxLw5");
+
+            app.UseFacebookAuthentication(
+               appId: "670219429798690",
+               appSecret: "437572a50582cc41545cfd23a4eeca37");
+            app.UseVkontakteAuthentication("5620808", "4BsCo5emmo3tqDdEraTF", "email");
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "266994273047-pht3i30br8hunmsu88hkk90imml18oh3.apps.googleusercontent.com",
+                ClientSecret = "BrUC_Lf0uR1-4ctJtTa7_3-T"
+            });
+        }
+    }
+}
